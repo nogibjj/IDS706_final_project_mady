fad36ed7fb962628b7e21f226727fc7e63588d23
GitHub Action <action@github.com>
2024-12-12 07:28:30 +0000

    rerun push

--- locust_test.py
+++ locust_test.py
@@ -1,12 +1,14 @@
 from locust import HttpUser, between, task
+
+
 class WebsiteUser(HttpUser):
     """
     Simulated user for testing the front page of the website.
     """
+
     host = "https://jdwhnfkggk.us-west-2.awsapprunner.com/"  # Replace with your website's URL if needed
     wait_time = between(2, 5)  # Wait between 2 to 5 seconds between requests
+
     @task
     def front_page(self):
         self.client.get("/")  # Test only the front page
-
-
